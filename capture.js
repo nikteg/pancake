dc3cbc5c9dcdd9c3fc8607517b43b7f704b0cd34
Niklas <me@sodapop.se>
2018-08-21 15:18:14 +0200

    Update screenshot script

--- capture.js
+++ capture.js
@@ -1,5 +1,4 @@
 const puppeteer = require("puppeteer")
-const devices = require("puppeteer/DeviceDescriptors")
 
 const captureScreenshot = async () => {
   const browser = await puppeteer.launch()
@@ -7,7 +6,7 @@ const captureScreenshot = async () => {
 
   await page.setViewport({ width: 1366, height: 768 })
   await page.goto("http://localhost:3000")
-  await page.screenshot({ path: `./docs/screenshot.png`, fullPage: true })
+  await page.screenshot({ path: "./docs/screenshot.png", fullPage: true })
 
   await browser.close()
 }
