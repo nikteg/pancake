dea5a76dfd886f1dc64d00487581d471d239823a
Niklas <me@sodapop.se>
2018-08-22 15:58:24 +0200

    Update screenshot

--- capture.js
+++ capture.js
@@ -4,7 +4,7 @@ const captureScreenshot = async () => {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
 
-  await page.setViewport({ width: 1366, height: 768 })
+  await page.setViewport({ width: 1366, height: 768, deviceScaleFactor: 2 })
   await page.goto("http://localhost:3000")
   await page.screenshot({ path: "./docs/screenshot.png", fullPage: true })
 
